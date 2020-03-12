7572d227a15f514297b82d7e3dd04174b9c5c4d7
marco <marrcogrova@gmail.com>
2020-03-12 18:33:07 +0100

    deps

--- scripts/install_dependencies_ci.sh
+++ scripts/install_dependencies_ci.sh
@@ -25,8 +25,6 @@ install_git_repo "nodeeditor"   "https://github.com/mico-corp/nodeeditor.git"
 install_git_repo "flow"         "https://github.com/mico-corp/flow.git"
 install_git_repo "core_mplugin" "https://github.com/mico-corp/core_mplugin.git"
 
-install_git_repo "Pangolin"     "https://github.com/stevenlovegrove/Pangolin.git"
-
 sudo apt-get install -y liblapack-dev libblas-dev libopenblas-dev libeigen3-dev
 sudo apt-get install -y libomp-dev libsuitesparse-dev libcholmod3
 
@@ -34,3 +32,5 @@ install_git_repo "DBoW2"         "https://github.com/dorian3d/DBoW2.git"
 install_git_repo "DLoopDetector" "https://github.com/dorian3d/DLoopDetector.git"
 install_git_repo "g2o"           "https://github.com/dorian3d/g2o.git"
 install_git_repo "slam_mplugin"  "https://github.com/mico-corp/slam_mplugin.git"
+
+install_git_repo "Pangolin"     "https://github.com/stevenlovegrove/Pangolin.git"
