86496b4e35dd8f15367ea3f53d044ae84e99fb2d
marco <marrcogrova@gmail.com>
2020-03-12 18:19:05 +0100

    added slam deps to travis

--- scripts/install_dependencies_ci.sh
+++ scripts/install_dependencies_ci.sh
@@ -24,5 +24,13 @@ install_git_repo () {
 install_git_repo "nodeeditor"   "https://github.com/mico-corp/nodeeditor.git"
 install_git_repo "flow"         "https://github.com/mico-corp/flow.git"
 install_git_repo "core_mplugin" "https://github.com/mico-corp/core_mplugin.git"
-install_git_repo "slam_mplugin" "https://github.com/mico-corp/slam_mplugin.git"
+
 install_git_repo "Pangolin"     "https://github.com/stevenlovegrove/Pangolin.git"
+
+sudo apt-get install -y liblapack-dev libblas-dev libopenblas-dev libeigen3-dev
+sudo apt-get install -y libomp-dev libsuitesparse-dev libcholmod3
+
+install_git_repo "DBoW2"         "https://github.com/dorian3d/DBoW2.git"
+install_git_repo "DLoopDetector" "https://github.com/dorian3d/DLoopDetector.git"
+install_git_repo "g2o"           "https://github.com/dorian3d/g2o.git"
+install_git_repo "slam_mplugin"  "https://github.com/mico-corp/slam_mplugin.git"
